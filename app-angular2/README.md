82254daa3b5499a36e595dc0add2e8ad838a59fc
congdh1 <congdh1@viettel.com.vn>
2018-09-10 16:41:45 +0700

    Init SPA app-angular2

---
+++ app-angular2/README.md
@@ -0,0 +1,4 @@
+app-angular2: Angular 2 Service Invocation Application
+===================================================
+
+SPA sử dụng Angular2 xác thực bằng Keycloak. Tham khảo từ example [app-angular2](https://github.com/keycloak/keycloak-quickstarts/tree/latest/app-angular2) của project `keycloak-quickstarts`
--- backend_oauth2_local_tokeninfo/app.py
+++ backend_oauth2_local_tokeninfo/app.py
@@ -18,7 +18,7 @@ def get_secret(user) -> str:
 
 
 def token_info(access_token) -> dict:
-    public_key = b'-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCrVrCuTtArbgaZzL1hvh0xtL5mc7o0NqPVnYXkLvgcwiC3BjLGw1tGEGoJaXDuSaRllobm53JBhjx33UNv+5z/UMG4kytBWxheNVKnL6GgqlNabMaFfPLPCF8kAgKnsi79NMo+n6KnSY8YeUmec/p2vjO2NjsSAVcWEQMVhJ31LwIDAQAB\n-----END PUBLIC KEY-----'
+    public_key = b'-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqlVDBYDyxuXK+5gzQCqvimQ11UqIXdPEXwn76r1ckTxb13SfRkgL/d/UYHUDPKo+uMbamonmqilHlCfHRZzu/S+1sc9x6FemH2me9MyO24uBHx9dZEmyhK2eVuIBJ/zx5F7gWODQ+kdLVz/be8y1CG+tr4ZCCVVw1xC5WB2fDpt/N3m7kwK+KKxjjiZokLGPK8N+Ahx1tCK+iuazNHfJCEGNjt+pzEcxe8QmL53GqWLwg6GsQkGn3cbOtKH9R8oq2nX6VLt/LBkZhYZybGKJ56bOVbtvE4QHrRIH6/6xnM7pj9l4O7O9WPH1cnyM6uwYajPR4OlAX1FiiCFE8rGgswIDAQAB\n-----END PUBLIC KEY-----'
     options = {'verify_aud': False}
     decoded = jwt.decode(access_token, public_key, algorithms='RS256', options=options)
     uid = decoded.get('preferred_username')
